f1ab9c6d7048e6c81f9330037d3d4745a327bf05
杨林 <lin897839@gmail.com>
2018-01-16 19:08:04 +0800

    Initial Commit

---
+++ NavgationAlphaDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NavgationAlphaDemo
+//
+//  Created by lin on 2018/1/16.
+//  Copyright © 2018年 YangLin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NavgationAlphaDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NavgationAlphaDemo
+//
+//  Created by lin on 2018/1/16.
+//  Copyright © 2018年 YangLin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
